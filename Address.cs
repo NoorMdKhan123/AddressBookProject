f554247aeff62d4eb2e64a04bace0dcd35602571
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-04 14:59:32 +0530

    Adding details one by one

---
+++ Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProject
+{
+    class Address
+    {
+        public string firstname;
+        public string lastname;
+        public string address;
+        public string city;
+        public string state;
+        public string zip;
+        public string phone;
+        public string email;
+        public Address(string firstname, string lastname, string address, string city,
+       string state, string zip, string phone, string email)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phone = phone;
+            this.email = email;
+        }
+    }
+}
---
+++ AddressBook.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProject
+{
+    class AddressBook
+    {
+        List<Address> addresses;
+
+        public AddressBook()
+        {
+            addresses = new List<Address>();
+        }
+
+        public bool add(string firstname, string lastname, string address, string city,
+            string state, string zip, string phone, string email)
+        {
+            Address addr = new Address(firstname, lastname, address, city,
+                state, zip, phone, email);
+
+            Address result = find(firstname);
+
+            if (result == null)
+            {
+                addresses.Add(addr);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool remove(string firstname)
+        {
+            Address addr = find(firstname);
+
+            if (addr != null)
+            {
+                addresses.Remove(addr);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void list(Action<Address> action)
+        {
+            addresses.ForEach(action);
+        }
+
+        public bool isEmpty()
+        {
+            return (addresses.Count == 0);
+        }
+
+        public Address find(string firstname)
+        {
+            Address addr = addresses.Find((a) => a.firstname == firstname);
+            return addr;
+        }
+
+    }
+}
+
